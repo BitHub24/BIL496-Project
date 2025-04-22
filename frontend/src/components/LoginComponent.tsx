1125347041935d3c5186b62f3e7ca27e40b45bc7
SteveGadd <janagircan@hotmail.com>
2025-04-22 15:38:05 +0300

    Add a loading animation to login screen.

--- frontend/src/components/LoginComponent.tsx
+++ frontend/src/components/LoginComponent.tsx
@@ -1,7 +1,22 @@
 import { useGoogleLogin } from "@react-oauth/google";
 import React, { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";  // Link import edilmesi gerekiyor
-import styled from "styled-components";
+import styled, { keyframes } from 'styled-components';
+
+const spin = keyframes`
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+`;
+
+const Loader = styled.div`
+  border: 4px solid #f3f3f3;
+  border-top: 4px solid #333;
+  border-radius: 50%;
+  width: 40px;
+  height: 40px;
+  animation: ${spin} 1s linear infinite;
+  margin: 2rem auto;
+`;
 
 // Styled components
 const LoginContainer = styled.div`
@@ -164,6 +179,7 @@ const Login = () => {
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
   const [success] = useState("");
+  const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
   const BACKEND_API_URL = import.meta.env.VITE_BACKEND_API_URL;
 
@@ -216,6 +232,7 @@ const Login = () => {
         const googleAuthResult = await res.json();
         console.log('Google Auth Result:', googleAuthResult);
         if (googleAuthResult.email) {
+          setLoading(true);
           const response = await fetch(`${BACKEND_API_URL}/api/users/google-auth/`, {
             method: "POST",
             headers: {
@@ -234,6 +251,9 @@ const Login = () => {
       } catch (error) {
         console.error('Error during Google login process:', error);
       }
+      finally{
+        setLoading(false);
+      }
     },
     onError: (error) => {
       console.error('Google Login Failed:', error);
@@ -242,62 +262,46 @@ const Login = () => {
   
   return (
     <LoginContainer>
-      <FormContainer>
-        <LogoContainer>
-          <Logo src="/landingLogo.png" alt="BitHub Logo" />
-        </LogoContainer>
-        <Form onSubmit={handleSubmit}>
-          <InputGroup>
-            <Label>Kullanıcı Adı</Label>
-            <Input
-              type="username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              required
-            />
-          </InputGroup>
-          <InputGroup>
-            <Label>Şifre</Label>
-            <Input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </InputGroup>
-          {error && <ErrorMessage>{error}</ErrorMessage>}
-          {success && <SuccessMessage>{success}</SuccessMessage>}
-          <Button type="submit">Giriş Yap</Button>
-        </Form>
-        <Divider>veya</Divider>
-        <GoogleButton onClick={() => googleLogin()}
-        >
-          <svg viewBox="0 0 24 24" width="18" height="18">
-            <path
-              fill="#4285F4"
-              d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"
-            />
-            <path
-              fill="#34A853"
-              d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z"
-            />
-            <path
-              fill="#FBBC05"
-              d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z"
-            />
-            <path
-              fill="#EA4335"
-              d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"
-            />
-          </svg>
-          &nbsp;Google ile Giriş Yap
-        </GoogleButton>
-        {/* Link to the Register Page */}
-        <p>Don't have an account? <Link to="/register">Create an account</Link></p>
-      </FormContainer>
+      {loading ? (
+        <Loader />
+      ) : (
+        <FormContainer>
+          <LogoContainer>
+            <Logo src="/landingLogo.png" alt="BitHub Logo" />
+          </LogoContainer>
+          <Form onSubmit={handleSubmit}>
+            <InputGroup>
+              <Label>Kullanıcı Adı</Label>
+              <Input
+                type="username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required
+              />
+            </InputGroup>
+            <InputGroup>
+              <Label>Şifre</Label>
+              <Input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </InputGroup>
+            {error && <ErrorMessage>{error}</ErrorMessage>}
+            {success && <SuccessMessage>{success}</SuccessMessage>}
+            <Button type="submit">Giriş Yap</Button>
+          </Form>
+          <Divider>veya</Divider>
+          <GoogleButton onClick={() => googleLogin()}>
+            {/* SVG icon */}
+            &nbsp;Google ile Giriş Yap
+          </GoogleButton>
+          <p>Don't have an account? <Link to="/register">Create an account</Link></p>
+        </FormContainer>
+      )}
     </LoginContainer>
-
   );
-};
+}
 
 export default Login;
