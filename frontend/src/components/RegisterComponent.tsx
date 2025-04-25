659a49f3b1e64fb99b0858b2cd9c507138ccb40d
pbucukoglu <pbucukoglu@etu.edu.tr>
2025-04-25 18:05:42 +0300

    Fixed frontend .env api url name

--- frontend/src/components/RegisterComponent.tsx
+++ frontend/src/components/RegisterComponent.tsx
@@ -142,7 +142,7 @@ const Register = () => {
     try {
       const { confirmPassword, ...data } = formData;
       console.log("Registering user:", { username: data.username, email: data.email, password: data.password });
-      const response = await fetch(`${import.meta.env.VITE_REACT_APP_BACKEND_API_URL}/api/users/register/`, {
+      const response = await fetch(`${import.meta.env.VITE_BACKEND_API_URL}/api/users/register/`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
